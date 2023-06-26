16920229fa41a6b1f8875783804e275cb6574cb6
Hadipermana12 <uciha.khadoetz10@gmail.com>
2023-06-26 11:22:29 +0700

    Memodifikasi DDL

--- src/ddl_sql_klinik.sql
+++ src/ddl_sql_klinik.sql
@@ -2,7 +2,7 @@ create database klinik;
 use klinik;
 
 create table jenis_pelayanan(id varchar(10), nama_pelayanan VARCHAR(30), nama_dokter varchar(30), tanggal DATE, hari VARCHAR(10), jam_buka TIME, jam_tutup TIME);
-create table loket(id_antrian VARCHAR(10), id_pelayanan VARCHAR(10), waktu_daftar TIMESTAMP);
+create table loket(id_antrian VARCHAR(10), id_pelayanan VARCHAR(10), waktu_daftar TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 create table antrian(id VARCHAR(10), nama_pasien VARCHAR(30), jenis_kelamin ENUM("P", "L"), umur INT(11), riwayat_penyakit varchar(20), nomor_antrian INT(11));
 
 alter table jenis_pelayanan add primary key (id);
